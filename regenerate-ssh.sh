92377b81bbdbab63dde2134d9b7fc733c00e10a2
Wojciech Marusiak <wojcieh@gmail.com>
2024-04-15 21:34:24 +0200

    Update regenerate-ssh.sh

--- regenerate-ssh.sh
+++ regenerate-ssh.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+#Set history to 10000
 set -o history   # Enable history expansion
 HISTFILE=~/.bash_history  # Set the location of the history file
 HISTSIZE=10000             # Increase history size (optional)
 
 #Clear hostname
-truncate -s0 /etc/hostname
-hostnamectl set-hostname localhost
+sudo truncate -s0 /etc/hostname
+sudo hostnamectl set-hostname localhost
 
 #Clear machine ID
-truncate -s0 /etc/machine-id
-rm /var/lib/dbus/machine-id
-ln -s /etc/machine-id /var/lib/dbus/machine-id
+sudo truncate -s0 /etc/machine-id
+sudo rm /var/lib/dbus/machine-id
+sudo ln -s /etc/machine-id /var/lib/dbus/machine-id
 
 #Clean cloud-init
 sudo cloud-init clean
+
+# Delete old ssh host keys 
+sudo /bin/rm -v /etc/ssh/ssh_host_*
+
+#Regenerate OpenSSH Host Keys
+sudo dpkg-reconfigure openssh-server
+
+#Restart SSH
+sudo systemctl restart ssh
