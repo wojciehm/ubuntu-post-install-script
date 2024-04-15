a8b1780256b67b99e51db1a91d3477038f898a26
Wojciech Marusiak <wojcieh@gmail.com>
2024-04-15 20:44:32 +0200

    Update post-install-script.sh

--- post-install-script.sh
+++ post-install-script.sh
@@ -16,3 +16,21 @@ export ZSH="$HOME/.oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # change ~/.zshrc file following
+
+# 1. Create the motd-backup folder in the user's home directory
+mkdir -p ~/motd-backup
+
+# 2. Move existing motd files to the backup folder
+sudo mv /etc/update-motd.d/* ~/motd-backup/
+
+# 3. Clone the GitHub repository
+git clone https://github.com/wojciehm/ansible
+
+# 4. Copy motd files from the repository
+sudo cp ansible/motd/* /etc/update-motd.d/
+
+# 5. Remove the cloned repository (optional for cleanup)
+rm -rf ansible
+
+# 6. Copy lolcat (you'll need to ensure lolcat is installed first)
+sudo cp /usr/games/lolcat /usr/bin
